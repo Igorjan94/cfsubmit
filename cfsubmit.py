37c03842ed978a324cd2f63902bf60ea0c89c2a0
cnt0 <sarum9n@gmail.com>
2015-05-05 05:25:04 +0500

    updated id codes etc

--- cfsubmit.py
+++ cfsubmit.py
@@ -9,9 +9,9 @@ import time
 # Edit these four variables according to your needs:
 x_user = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 csrf_token = "yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy"
-cf_domain = "com"  # "ru" if you prefer codeforces.ru
+cf_domain = "ru"  # "com" if you prefer codeforces.com
 ext_id = {
-    "cpp":   "16",
+    "cpp":   "42",
     "cs":    "29",
     "go":    "32",
     "java":  "36",
@@ -19,39 +19,44 @@ ext_id = {
 }
 
 # programTypeId - Language
-# 1: GNU G++ 4.9.2
-# 10: GNU GCC 4.9.2
-# 12: Haskell GHC 7.6
-# 13: Perl 5.12
-# 14: ActiveTcl 8.5
-# 15: Io-2008-01-07 (Win32)
-# 16: GNU G++11 4.9.2
-# 17: Pike 7.8
-# 18: Befunge
-# 19: OCaml 4
-# 2: Microsoft Visual C++ 2010
-# 20: Scala 2.11
-# 22: OpenCobol 1.0
-# 23: Java 7
-# 25: Factor
-# 26: Secret_171
-# 27: Roco
-# 28: D DMD32 Compiler v2
-# 29: MS C# .NET 4
-# 3: Delphi 7
-# 31: Python 3.4
-# 32: Go 1.2
-# 33: Ada GNAT 4
-# 34: JavaScript V8 3
-# 36: Java 8
-# 38: Mysterious Language
-# 39: FALSE
-# 4: Free Pascal 2
-# 5: Java 6
-# 6: PHP 5.3
-# 7: Python 2.7
-# 8: Ruby 2
-# 9: C# Mono 2.1
+# 1:   GNU G++ 4.9.2
+# 10:  GNU GCC 4.9.2
+# 12:  Haskell GHC 7.6
+# 13:  Perl 5.12
+# 14:  ActiveTcl 8.5
+# 15:  Io-2008-01-07 (Win32)
+# 17:  Pike 7.8
+# 18:  Befunge
+# 19:  OCaml 4
+# 2:   Microsoft Visual C++ 2010
+# 20:  Scala 2.11
+# 22:  OpenCobol 1.0
+# 23:  Java 7
+# 25:  Factor
+# 26:  Secret_171
+# 27:  Roco
+# 28:  D DMD32 Compiler v2
+# 29:  MS C# .NET 4
+# 3:   Delphi 7
+# 31:  Python 3.4
+# 32:  Go 1.2
+# 33:  Ada GNAT 4
+# 34:  JavaScript V8 3
+# 36:  Java 8
+# 38:  Mysterious Language
+# 39:  FALSE
+# 4:   Free Pascal 2
+# 40:  PyPy 2.5.0
+# 41:  PyPy 3.2.5
+# 42:  GNU G++11 4.9.2
+# 43:  GNU GCC C11 4.9.2
+# 44:  Picat 0.9
+# 45:  GNU C++ 11 ZIP
+# 46:  Java 8 ZIP
+# 6:   PHP 5.3
+# 7:   Python 2.7
+# 8:   Ruby 2
+# 9:   C# Mono 2.10
 
 
 def is_gym(contest_id):
